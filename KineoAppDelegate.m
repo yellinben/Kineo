5f39da28d9b25b82c240f9cd047f5026034be782
Ben Yellin <YellinBen@gmail.com>
2011-03-27 18:56:27 -0400

    fixed merge issues

--- KineoAppDelegate.m
+++ KineoAppDelegate.m
@@ -13,13 +13,7 @@
 @implementation KineoAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-<<<<<<< HEAD
 	[[CameraWindowController sharedCameraWindow] showWindow:nil];
-=======
-	cameraController = [[CameraWindowController alloc] 
-						initWithWindowNibName:@"Camera"];
-	[cameraController showWindow:nil];
->>>>>>> 4bc11762c3e0d0afced952a438aa4ca3b653c33f
 	
 	// Register Default Prefs
 	NSDictionary *defaultPrefs = [NSDictionary dictionaryWithObjectsAndKeys:
@@ -30,13 +24,10 @@
 
 - (IBAction)showPreferences:(id)sender {
 	[[PreferencesController sharedPreferences] showWindow:nil];
-<<<<<<< HEAD
 }
 
 - (IBAction)showCameraWindow:(id)sender {
 	[[CameraWindowController sharedCameraWindow] showWindow:nil];
-=======
->>>>>>> 4bc11762c3e0d0afced952a438aa4ca3b653c33f
 }
 
 @end
